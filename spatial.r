60a001464e7c4de1964af35898528712d636ffdc
Ryan Naughton <rnaughton@taximagic.com>
2013-05-09 23:17:59 -0400

    added data

--- spatial.r
+++ spatial.r
@@ -1,6 +1,7 @@
+library(foreign)
 library(sp)
 library(rgdal)
-library('sm')
+library(sm)
 library(shapefiles)
 library(rgeos)
 
@@ -11,5 +12,10 @@ costa_rica_sp_2 <- readOGR("CRI_adm/",layer="CRI_adm2")
 
 save(costa_rica_sp_0,costa_rica_sp_1,costa_rica_sp_2,file="cr_spatial_polygons.rdata")
 
+plot(costa_rica_sp_2)
 
-plot(costa_rica_sp)
+head(costa_rica_sp_1@data)
+head(costa_rica_sp_2@data)
+
+#load infrastructure data
+data <- read.dta(file="data_final.dta")
