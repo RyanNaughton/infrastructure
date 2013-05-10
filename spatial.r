4fe234d1cbbb822e92d9de638aba7b3831f8ac88
Ryan Naughton <rnaughton@taximagic.com>
2013-05-09 23:04:01 -0400

    adding spatialpolygons data in R form bc july's R wont read the shapefiles.

--- spatial.r
+++ spatial.r
@@ -5,6 +5,11 @@ library(shapefiles)
 library(rgeos)
 
 
-costa_rica_sp <- readOGR("CRI_adm/",layer="CRI_adm0")
+costa_rica_sp_0 <- readOGR("CRI_adm/",layer="CRI_adm0")
+costa_rica_sp_1 <- readOGR("CRI_adm/",layer="CRI_adm1")
+costa_rica_sp_2 <- readOGR("CRI_adm/",layer="CRI_adm2")
+
+save(costa_rica_sp_0,costa_rica_sp_1,costa_rica_sp_2,file="cr_spatial_polygons.rdata")
+
 
 plot(costa_rica_sp)
